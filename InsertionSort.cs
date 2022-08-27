5c4f232c4d470ef9b51d7af323da9402286e29d9
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-28 00:56:07 +0530

    Insertion sorting done

---
+++ InsertionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortDemo
+{
+    public class InsertionSort
+    {
+        public void Checksorting()
+        {
+            int[] arr = { 23, 9, 85, 12 };
+
+            int n = 4, i, j, val, flag;
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1; i++)
+                {
+                    if(val < arr[j])
+                    {
+                        arr[j+1] = arr[j];
+                        j--;
+                        arr[j+1] = val;
+                    }
+                    else
+                    {
+                        flag = 1;
+                    }
+                }
+            }
+            Console.WriteLine("Array is sorted: ");
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BubbleSortDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BubbleSort bubbleSort = new BubbleSort();
+            //bubbleSort.Sorting();
+
+            //Anagram anagram = new Anagram();
+            //anagram.checkAnagram();
+
+            InsertionSort insertionSort = new InsertionSort();  
+            insertionSort.Checksorting();
+        }
+    }
+}
+
+//Comparion on orting techniques - 
